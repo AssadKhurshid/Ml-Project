d983d5ccfb029682f951be1c45ff771892b2b355
AssadKhurshid <assadkhurshid91@gmail.com>
2025-06-27 18:13:23 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='Ml-Project',
+    version='0.1.0',
+    author='Assad Khurshid',
+    author_email='assadkhurshid91@gmail.com',
+    install_requires=['numpy', 'pandas', 'scikit-learn', 'matplotlib', 'seaborn'],
+)
