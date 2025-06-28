54dff4f5544986e6f36426a44cdd01ff34d50043
AssadKhurshid <assadkhurshid91@gmail.com>
2025-06-28 23:48:59 +0200

    EDA and Problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 
 # Custom exception module to handle errors in the application
@@ -20,8 +20,13 @@ def error_message_detail(error, error_detail: sys):
 class CustomException(Exception):
     def __init__(self, error_message: str, error_detail: sys):
         super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+        self.error_message = error_message_detail(
+            error_message, error_detail=error_detail
+        )
 
     def __str__(self):
         return self.error_message
+
+
+
 
--- src/logger.py
+++ src/logger.py
@@ -1,9 +1,9 @@
 import logging
-import os 
+import os
 from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d')}.log"
-logs_path=os.path.join(os.getcwd(), 'logs', LOG_FILE)
+logs_path = os.path.join(os.getcwd(), 'logs', LOG_FILE)
 os.makedirs(logs_path, exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
@@ -12,10 +12,5 @@ logging.basicConfig(
     filename=LOG_FILE_PATH,
     format='%(asctime)s - %(levelname)s - %(message)s',
     level=logging.INFO,
-    
-    
 )
 
-
-if __name__ == "__main__":
-    logging.info("Logging Has Started.")
